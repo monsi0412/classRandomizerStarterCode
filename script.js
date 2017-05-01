bdfcab74af525ee74ec5174a3b92d0eeaee974fd
Monserrat <monserratdiaz12@gmail.com>
2017-05-01 12:34:56 +0000

    wrote java

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+/*global $*/
+/*global math*/
+
 $(document).ready(function(){
 
+var students=[
+    "Monserrat", "Leslie", "Adonis" 
+];
+var teachers=[
+    "Aaron", "Zack", "Justin", "Alyxe", "Julia"
+];
 
+var randomStudent=math.floor(math.random()*students.length);
+var randomTeacher=math.floor(math.random()*teachers.length);
+
+$("#studentButton").click(function(){
+    ("#studentDisplay").append(students[randomStudent]);
+});
+$("#teacherButton").click(function(){
+    ("teacherDisplay").append(teachers[randomTeacher]);
+});
 
 });
